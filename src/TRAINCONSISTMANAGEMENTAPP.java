98dee2ba609a4031e0047b47ea0d57f419e13142
C-Vaishnavi2005 <vaishnavi.csekar@gmail.com>
2026-03-27 09:34:09 +0530

    UC2:Hardcoded palindrome

--- src/TRAINCONSISTMANAGEMENTAPP.java
+++ src/TRAINCONSISTMANAGEMENTAPP.java
@@ -1,17 +1,16 @@
 import java.util.*;
+import java.util.stream.*;
 
 // Passenger Bogie class
 class PassengerBogie {
     String name;
     int capacity;
 
-    // Constructor
     PassengerBogie(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
 
-    // Display method
     public void display() {
         System.out.println(name + " -> Capacity: " + capacity);
     }
@@ -21,26 +20,22 @@ public class TRAINCONSISTMANAGEMENTAPP {
 
     public static void main(String[] args) {
 
-        System.out.println("===== Train Consist Management App (UC7) =====");
+        System.out.println("===== Train Consist Management App (UC8) =====");
 
         // Step 1: Create list
         List<PassengerBogie> bogies = new ArrayList<>();
-
-        // Step 2: Add objects
         bogies.add(new PassengerBogie("Sleeper", 72));
         bogies.add(new PassengerBogie("AC Chair", 56));
         bogies.add(new PassengerBogie("First Class", 24));
 
-        // Step 3: Sort using Comparator (by capacity)
-        Collections.sort(bogies, new Comparator<PassengerBogie>() {
-            public int compare(PassengerBogie b1, PassengerBogie b2) {
-                return b1.capacity - b2.capacity; // ascending
-            }
-        });
+        // Step 2: Convert to stream & filter (capacity > 50)
+        List<PassengerBogie> filteredBogies = bogies.stream()
+                .filter(b -> b.capacity > 50)
+                .collect(Collectors.toList());
 
-        // Step 4: Display sorted bogies
-        System.out.println("Bogies sorted by capacity:");
-        for (PassengerBogie b : bogies) {
+        // Step 3: Display filtered bogies
+        System.out.println("High Capacity Bogies (capacity > 50):");
+        for (PassengerBogie b : filteredBogies) {
             b.display();
         }
 
